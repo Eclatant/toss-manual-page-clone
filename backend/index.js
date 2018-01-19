2c6aeac8c2a01b70deb55c477973e3a70bf382bb
Jeewhan <distinct19@gmail.com>
2018-01-19 14:03:25 +0900

    morgan을 logging을 위해 설치

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const { port } = require("./config");
 const manual = require("./routes/manual");
 
 const app = express();
 
+app.use(morgan("dev"));
 app.use("/manual", manual);
 
 app.listen(port, () => {
