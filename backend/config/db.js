a6a7ceaf4d5ffd90f08c0de16c30a5de120c96e6
Jeewhan <distinct19@gmail.com>
2018-01-19 18:09:34 +0900

    DB 접속시에 Seed Data Insert

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,30 @@
 const mongoose = require("mongoose");
 
 const { dbUri } = require("./index");
+const Manual = require("../models/manual");
+const manuals = require("../data/seed");
 
 module.exports = () => {
   mongoose.Promise = global.Promise;
 
-  const db = mongoose.connection;
-  db.on(`error`, console.error);
-  db.once(`open`, () => {
+  const connection = mongoose.connection;
+  connection.on(`error`, console.error);
+  connection.once(`open`, () => {
     console.log("MongoDB connect");
   });
 
-  const connect = mongoose.connect(dbUri);
+  mongoose
+    .connect(dbUri)
+    .then(_ =>
+      connection.db.dropCollection("manuals", error => {
+        if (error) console.error(`dropCollection error ${error}`);
+      })
+    )
+    .then(_ => {
+      for (m of manuals)
+        Manual(m).save(error => {
+          if (error) console.error(`Document Create Error ${error}`);
+        });
+    })
+    .catch(reason => console.error(reason));
 };
