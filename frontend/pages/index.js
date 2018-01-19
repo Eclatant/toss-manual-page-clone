88b288785f47234cea089ce99bc57d8aa9e0e225
Jeewhan <distinct19@gmail.com>
2018-01-19 21:06:14 +0900

    Data Fetch 테스트

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,12 +1,28 @@
 import React from "react";
 import Head from "next/head";
 
-export default () => (
-  <div>
-    <Head>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <title>토스 결제 시작하기</title>
-    </Head>
-    Welcome to next.js!
-  </div>
-);
+import "isomorphic-unfetch";
+
+export default class Index extends React.Component {
+  static async getInitialProps() {
+    const { stargazers_count: stars } = await fetch(
+      "https://api.github.com/repos/zeit/next.js"
+    ).then(v => v.json());
+    return { stars };
+  }
+
+  render() {
+    return (
+      <div>
+        <Head>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <title>토스 결제 시작하기</title>
+        </Head>
+        <p>Next.js has {this.props.stars} ⭐️</p>
+      </div>
+    );
+  }
+}
