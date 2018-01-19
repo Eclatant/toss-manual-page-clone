d790663cf43f9a7d3d9f6d4638a53967c8139a8f
Jeewhan <distinct19@gmail.com>
2018-01-19 18:08:39 +0900

    Test를 위한 Seed Data 생성

---
+++ backend/data/seed.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    title: `### 가입부터 결제 완료까지`,
+    content: `#### 0. 당신은 온라인 상점 '토스몰'을 열었습니다.`
+  },
+  {
+    title: `### 결제 생성하기`,
+    content: `#### 결제 생성 API의 Endpoint`
+  }
+];
