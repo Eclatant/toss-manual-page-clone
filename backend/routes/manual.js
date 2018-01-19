bab20c23b7a3dfa58196416cf75e4488d99b96f0
Jeewhan <distinct19@gmail.com>
2018-01-19 16:42:05 +0900

    GET /manual Manuals Collection 전체를 json으로 res

--- backend/routes/manual.js
+++ backend/routes/manual.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
+const Manual = require("../models/manual.js");
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({ contents: "" });
+  Manual.find({}, (error, manuals) => res.json(error ? error : { manuals }));
 });
 
 router.post("/", ({ body }, res) => {
