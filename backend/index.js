174a89dcf1b50613f7683335d93611dd68a7b352
Jeewhan <distinct19@gmail.com>
2018-01-19 13:55:19 +0900

    GET /manual API 초기화

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 
 const { port } = require("./config");
+const manual = require("./routes/manual");
 
 const app = express();
+
+app.use("/manual", manual);
 
 app.listen(port, () => {
   console.log(`Hello, ${process.env.USER}! Server is running on ${port} port!`);
