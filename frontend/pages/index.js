45e3fb92637102d8fcd6050fe4ad571343547e04
Jeewhan <distinct19@gmail.com>
2018-01-19 20:47:25 +0900

    react react-dom next 설치
    * .gitignore에 .next 추가

---
+++ frontend/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Head from "next/head";
+
+export default () => (
+  <div>
+    <Head>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <title>토스 결제 시작하기</title>
+    </Head>
+    Welcome to next.js!
+  </div>
+);
