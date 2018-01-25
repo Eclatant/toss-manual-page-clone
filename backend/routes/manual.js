cb66cbcbe3f9e7cc703d246d1f4ed20dffdc60c4
Jeewhan <distinct19@gmail.com>
2018-01-25 15:04:11 +0900

    기존 API 관련 수정
    * manual 생성시 order 추가
    * POST에 대한 리턴값 변경
    * PUT에서 contentId가 아닌 order로 처리

--- backend/routes/manual.js
+++ backend/routes/manual.js
@@ -4,43 +4,53 @@ const Manual = require("../models/manual.js");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  Manual.find({}, (error, manuals) => res.json(error ? error : { manuals }));
-});
+router.get("/", (req, res) =>
+  Manual.find({}, (error, manuals) => res.json(error ? error : { manuals }))
+);
 
-router.post("/", ({ body }, res) => {
+router.post("/", ({ body, body: { value } }, res) => {
   console.log(JSON.stringify(body, null, 2));
-  ({ value } = body);
-  const manual = Manual({ value });
-  manual.save(error => res.json(error ? error : { complete: true }));
+
+  Manual.count({}, (error, order) => {
+    if (error) {
+      console.error(`Collection Count Error ${error}`);
+      return res.status(500).json({ complete: false });
+    }
+
+    const m = Manual({ value, order });
+
+    m.save(error => {
+      if (error) {
+        console.error(`Document Create Error ${error}`);
+        return res.status(416).json({ complete: false });
+      }
+
+      return res.status(200).json({ complete: true });
+    });
+  });
 });
 
-router.put("/:contentId", ({ body, params }, res) => {
-  Manual.findOneAndUpdate(
-    { id: params.contentId },
-    { value: body.value },
-    {},
-    (error, manual) => {
-      if (error) {
-        console.error(`database failure ${error}`);
-        return res.status(500).json({ complete: false });
-      }
-
-      if (!manual) {
-        console.error(`manual not found`);
-        return res.status(404).json({ complete: false });
-      }
-
-      manual.save(error => {
-        if (error) {
-          console.error(`Document Create Error ${error}`);
-          res.status(416).json({ complete: false });
-        } else {
-          res.status(200).json({ complete: true });
-        }
-      });
+router.put("/:order", ({ body: { value }, params: { order } }, res) => {
+  Manual.findOneAndUpdate({ order }, { value }, (error, manual) => {
+    if (error) {
+      console.error(`database failure ${error}`);
+      return res.status(500).json({ complete: false });
     }
-  );
+
+    if (!manual) {
+      console.error(`manual not found`);
+      return res.status(404).json({ complete: false });
+    }
+
+    manual.save(error => {
+      if (error) {
+        console.error(`Document Create Error ${error}`);
+        res.status(416).json({ complete: false });
+      } else {
+        res.status(200).json({ complete: true });
+      }
+    });
+  });
 });
 
 router.delete("/:order", ({ params: { order } }, res) => {
