47cc1008304e3e47c1c6e23439d002b66d8b9006
Jeewhan <distinct19@gmail.com>
2018-01-25 21:21:43 +0900

    spoqa han sans font 불러오기

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -5,6 +5,11 @@ const Layout = ({ children }) => (
     <Head>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <title>토스 결제 시작하기</title>
+      <link
+        href="//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-kr.css"
+        rel="stylesheet"
+        type="text/css"
+      />
     </Head>
     {children}
   </div>
